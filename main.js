50cea8aa66a40fc9ea74dea5d1e501d360cd4ab2
sivaaravindh <sivaaravind262003@gmail.com>
2022-12-16 22:02:37 -0800

    index

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let a=parseInt (prompt("Enter the value"));
-
-for (var x=1; x<=a; x++) {
+let n=0;
+/*for (var x=1; x<=a; x++) {
     
 
          if (x % 2 === 0) {
                 document.write(x + " is even"+"<br>");   
     }
     
-}
+}*/
  
+for (var x=1; x<=a; x++) {
+    
+
+    n=n+2;
+    document.write(n+"<br>")
+}
